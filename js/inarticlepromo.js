4c0af8dcc1b4f00494dc6f41d2cb91fc713c550f
Daniel J. Schneider <dan@schneidan.com>
2017-01-24 17:09:27 -0700

    Change to using https for the extras link

--- js/inarticlepromo.js
+++ js/inarticlepromo.js
@@ -2,6 +2,6 @@ javascript:(function () {
     var dt = new Date();
     var secs = dt.getSeconds() + (60 * dt.getMinutes());
     var bookmarletSource = document.createElement('script'); 
-    bookmarletSource.setAttribute('src', 'http://extras.denverpost.com/app/bookmarklet/js/inarticlepromo-src.js?v='+secs);
+    bookmarletSource.setAttribute('src', 'https://extras.denverpost.com/app/bookmarklet/js/inarticlepromo-src.js?v='+secs);
     document.body.appendChild(bookmarletSource); 
 }());

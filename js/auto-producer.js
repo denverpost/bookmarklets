2fd6d36d893fb1baff073d9782bd183f0776268a
Daniel J. Schneider <dan@schneidan.com>
2017-02-03 12:05:56 -0700

    Auto-Producer added and Index updated

--- js/auto-producer.js
+++ js/auto-producer.js
@@ -1,5 +1,57 @@
 javascript:(function() {
-    var selectFunction = prompt('Welcome to AUTO-PRODUCER. How can I help?\n\n\
-            (1) Trump story helper\n\n\
-            Enter selection:\n','');
+    function isValid(str) {
+        var string = parseInt(str);
+        if (string >= 0 && strong <= 99) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function checkTagBoxes(tags){
+        for(var i=0,len=tags.length;i<len;i++){
+            document.getElementById(tags[i]).checked = true;
+        }
+    }
+
+    function primaryOptions(sectionPrimary,tagPrimary){
+        var sectionSelect = 'fm-mason_post_settings-0-schema-0-primary_section-0';
+        var tagSelect = 'fm-mason_post_settings-0-schema-0-primary_tag-0';
+        document.getElementById(sectionSelect).value = sectionPrimary;
+        document.getElementById(tagSelect).value = tagPrimary;
+    }
+
+    function addTag(newTags){
+        for(var i=0,len=newTags.length;i<len;i++){
+            document.getElementById('new-tag-post_tag').value = newTags[i];
+            var button = jQuery('#post_tag .ajaxtag input.button.tagadd').click();
+        }
+    }
+
+    function trumpThatBitch() {
+        var tags = ['in-category-11580', 'in-category-75', 'in-category-81', 'in-category-48', 'in-category-39', 'in-category-59'];
+        var newTags = ['Donald Trump'];
+        var sectionPrimary = '11580';
+        var tagPrimary = '1276';
+        checkTagBoxes(tags);
+        addTag(newTags);
+        primaryOptions(sectionPrimary,tagPrimary)
+    }
+
+    loop:
+    while(true) {
+        var selectFunction = prompt('Welcome to AUTO-PRODUCER. How can I help?\n\n\n\
+            ( 1 ) Trump story helper\n\n\n\
+        Enter selection:\n','0');
+        switch (selectFunction) {
+            case '1':
+                trumpThatBitch();
+                break loop;
+            default:
+                var again = confirm('That\'s not a valid option. Try again?');
+                if (again == true) {
+                    break loop;
+                }
+        }
+    }
 }());

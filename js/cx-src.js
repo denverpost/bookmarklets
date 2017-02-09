eb9cceb2a8c78085985da46f961dcb9179f0f63e
Daniel J. Schneider <dan@schneidan.com>
2017-02-09 12:05:37 -0700

    Isolated the CX bookmarklet's functions.

--- js/cx-src.js
+++ js/cx-src.js
@@ -1,17 +1,19 @@
-var content = jQuery('#content');
-var grafs = content.text().split('\n\n');
+javascript:(function() {
+	var content = jQuery('#content');
+	var grafs = content.text().split('\n\n');
 
-var cx_date = new Date();
-var monthNames = ["Jan. ","Feb. ","March ","April ","May ","June ","July ","Aug. ","Sept. ","Oct. ","Nov.","Dec. "];
-var cx_month = monthNames[cx_date.getMonth()];
-var amPm = ( cx_date.getHours() < 12 ) ? ' a.m.' : ' p.m.';
-var cx_date_markup = cx_month +  cx_date.getDate() + ', ' + cx_date.getFullYear() + ' at ' + cx_date.getHours() + ':' + cx_date.getMinutes() + amPm;
+	var cx_date = new Date();
+	var monthNames = ["Jan. ","Feb. ","March ","April ","May ","June ","July ","Aug. ","Sept. ","Oct. ","Nov.","Dec. "];
+	var cx_month = monthNames[cx_date.getMonth()];
+	var amPm = ( cx_date.getHours() < 12 ) ? ' a.m.' : ' p.m.';
+	var cx_date_markup = cx_month +  cx_date.getDate() + ', ' + cx_date.getFullYear() + ' at ' + cx_date.getHours() + ':' + cx_date.getMinutes() + amPm;
 
-var cx_content = prompt('Type or paste the correction language here. The Update date and time and all other formatting will be included automatically.\n\n', '');
+	var cx_content = prompt('Type or paste the correction language here. The Update date and time and all other formatting will be included automatically.\n\n', '');
 
-var markup = '<hr />\n\n\
-<strong>Updated ' + cx_date_markup + '</strong> <em>' + cx_content + '</em>';
+	var markup = '<hr />\n\n\
+	<strong>Updated ' + cx_date_markup + '</strong> <em>' + cx_content + '</em>';
 
-grafs.splice(grafs.length, 0, markup);
+	grafs.splice(grafs.length, 0, markup);
 
-jQuery('#content').text(grafs.join('\n\n'));
+	jQuery('#content').text(grafs.join('\n\n'));
+}());

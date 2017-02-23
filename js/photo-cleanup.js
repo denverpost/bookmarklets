dd8e8c6e3b150b23d5fa3e37a57c3a512ce417a2
Daniel J. Schneider <dan@schneidan.com>
2017-02-23 13:24:42 -0700

    Big improvements to credit detection in photo cleanup

--- js/photo-cleanup.js
+++ js/photo-cleanup.js
@@ -1,18 +1,13 @@
 javascript:
 (function() {
+	var locate = window.location.href;
 	function trim_words(theString, numWords) {
-        expString = theString.split(/\s+/,numWords);
-        theNewString=expString.join(" ");
+        var expString = theString.split(/\s+/,numWords);
+        var theNewString = expString.join(' ');
         return theNewString;
     }
     function abbrevMonths(captionTextAbbrev) {
-    	return captionTextAbbrev.replace(' January ',' Jan. ').replace(' February ',' Feb. ').replace(' August ',' Aug. ').replace(' September ',' Sept. ').replace(' October ',' Oct. ').replace(' November ',' Nov. ').replace(' December ',' Dec. ')
-    }
-    var titleParent = document.querySelectorAll('[data-setting="title"] input[type="text"]');
-    var title = titleParent[0];
-    titleText = title.value;
-    if (titleText.match(/APTOPIX/)) {
-    	title.value = titleText.replace('APTOPIX ','');
+    	return captionTextAbbrev.replace(' January ',' Jan. ').replace(' February ',' Feb. ').replace(' August ',' Aug. ').replace(' September ',' Sept. ').replace(' October ',' Oct. ').replace(' November ',' Nov. ').replace(' December ',' Dec. ');
     }
     var captionParent = document.querySelectorAll('[data-setting="caption"] textarea');
     var caption = captionParent[0];
@@ -24,33 +19,41 @@ javascript:
     description.value = captionText;
     var creditParent = document.querySelectorAll('.compat-field-credit td.field input[type="text"]');
     var credit = creditParent[0];
-    var re = /\(([^.\(]{4,})\)/;
+    var re = /\(([^\(\)]{4,})\)/;
+    var photoCred = '';
+    var captionTextNew = '';
 	if (document.body.classList.contains('modal-open')) {
-        var photoCred = captionText.match(re)[1];
-        var captionTextNew = captionText.replace('('+photoCred+')','').replace(', Colorado.','.').trim();
+        photoCred = captionText.match(re)[1];
+        captionTextNew = captionText.replace('('+photoCred+')','').replace(', Colorado.','.').trim();
         captionTextNew = abbrevMonths(captionTextNew);
+        var photoCredNew = '';
         if (photoCred.match(/AP Photo by/)) {
         	photoCredNew = photoCred.replace('AP Photo by ','') + ', The Associated Press';
         } else if (photoCred.match(/AP Photo\//)) {
         	photoCredNew = photoCred.replace('AP Photo/','') + ', The Associated Press';
-        } else if (photoCred.match(/Photo by/)) {
-        	photoCredNew = photoCred.replace('Photo by ','').replace('/',', ');
         } else if (photoCred.match(/Photo By/)) {
         	photoCredNew = photoCred.replace('Photo By ','').replace('/',', ');
+        } else if (photoCred.match(/Photo by/)) {
+        	photoCredNew = photoCred.replace('Photo by ','').replace('/',', ');
         }
         credit.value = photoCredNew;
-    } else if (loc.indexOf('upload.php') > -1) {        
-        var photoCred = (re.test(captionText)) ? '('+captionText.match(re)[1]+')' : '';
-        var captionTextNew = captionText.replace(photoCred,'').replace(', Colorado.','.').trim();
+    } else if (locate.indexOf('upload.php') > -1) {
+        photoCred = '('+captionText.match(re)[1]+')';
+        captionTextNew = captionText.replace(photoCred,'').replace(', Colorado.','.').trim();
         captionTextNew = abbrevMonths(captionTextNew);
     }
     var dateline = captionTextNew.substring(0,captionTextNew.indexOf(':'));
-    if (dateline.length == 0 || /[a-z]/.test(dateline) == false) {
+    if (dateline.length === 0 || /[a-z]/.test(dateline) === false) {
         captionTextNew = captionTextNew.replace(dateline,'').replace(':','').trim();
         caption.value = captionTextNew;
     }
     var altText = trim_words(captionTextNew,6) + ' ...';
     alt.value = altText;
     var titleParent = document.querySelectorAll('[data-setting="title"] input[type="text"]');
-    titleParent.focus;
+    var title = titleParent[0];
+    var titleText = title.value;
+    if (titleText.match(/APTOPIX/)) {
+    	title.value = titleText.replace('APTOPIX ','');
+    }
+    altParent.focus;
 });
